e1003a77e43fa5f6e71548a44b9d93be99d89cc7
DesertOvercrowded <desertovercrowded@gmail.com>
2026-06-28 01:00:04 +0200

    chore: add OpenWeather configuration (#4)

--- backend/src/main/java/com/skyluma/weather/SkyLumaApplication.java
+++ backend/src/main/java/com/skyluma/weather/SkyLumaApplication.java
@@ -1,13 +1,15 @@
 package com.skyluma.weather;
 
+import com.skyluma.weather.config.OpenWeatherProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(OpenWeatherProperties.class)
 public class SkyLumaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SkyLumaApplication.class, args);
     }
-
 }
