b0f997599780d24443cd351183567f0d09c06d0e
CyberTerrorism <desertovercrowded@email.com>
2026-06-28 00:18:53 +0200

    chore: initialize Spring Boot backend

---
+++ backend/src/main/java/com/skyluma/weather/SkyLumaApplication.java
@@ -0,0 +1,13 @@
+package com.skyluma.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyLumaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyLumaApplication.class, args);
+    }
+
+}
